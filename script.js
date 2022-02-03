0e5d65eeb0cc085c52a5be1d33488de7fcd347df
GriffinPixel <griffinpixelbot@gmail.com>
2022-02-03 18:16:25 +0700

    update google map

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(window).scroll(function () {
     if ($(this).scrollTop() > 106) {
-        $(".index-title-wrapper").show(100);
-        $(".griffinpixel-wrapper").show(100);
+        $(".index-title-wrapper").show(80);
+        $(".griffinpixel-wrapper").show(80);
     }
     else {
-        $(".index-title-wrapper").hide(100);
-        $(".griffinpixel-wrapper").hide(100);
+        $(".index-title-wrapper").hide(80);
+        $(".griffinpixel-wrapper").hide(80);
     }
 });
